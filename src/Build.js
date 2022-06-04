d490db253b016ddb81be0c137a9e66b18fcaf613
lobomfz <lobomfz@protonmail.com>
2022-06-04 02:07:24 -0300

    build

---
+++ src/Build.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const TOKEN = process.env.TOKEN;
+
+exports.get = (req, res, next) => {
+	axios
+		.get(
+			'https://lobomfz.com/jenkins/job/Self%20Building%20Resume%20Pipeline/lastBuild/buildNumber',
+			{
+				auth: {
+					username: 'lobomfz',
+					password: TOKEN,
+				},
+			}
+		)
+		.then((response) => {
+			res.json(response.data);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
--- src/Route.js
+++ src/Route.js
@@ -1,5 +1,7 @@
-const Controller = require('./Controller');
+const Console = require('./Console');
+const Build = require('./Build');
 
 module.exports = (app) => {
-	app.get('/console', Controller.get);
+	app.get('/api/console', Console.get);
+	app.get('/api/build', Build.get);
 };
