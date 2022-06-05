25637734477d0dd01a55ae2dfcecb9db202ac554
lobomfz <lobomfz@protonmail.com>
2022-06-04 21:52:56 -0300

    rate limiter

--- src/Route.js
+++ src/Route.js
@@ -1,9 +1,18 @@
 const Console = require('./Console');
 const Build = require('./Build');
 const NewBuild = require('./NewBuild');
+const rateLimit = require('express-rate-limit');
+
+const rateLimiter = rateLimit({
+	windowMs: 5 * 60 * 1000,
+	max: 1,
+	message: 'Too many build requests.',
+	standardHeaders: true,
+	legacyHeaders: false,
+});
 
 module.exports = (app) => {
 	app.get('/api/console', Console.get);
 	app.get('/api/build', Build.get);
-	app.post('/api/newbuild', NewBuild.post);
+	app.post('/api/newbuild', rateLimiter, NewBuild.post);
 };
